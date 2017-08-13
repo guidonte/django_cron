ba30b8626b88e3ed8ecb1d4014849e34b45e844a
Guido Amoruso <guidonte@gmail.com>
2017-08-13 09:59:51 +0100

    Fix package name.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='django',
+    name='django_cron',
     install_requires=['django'],
     zip_safe=False,
     packages=find_packages(),
